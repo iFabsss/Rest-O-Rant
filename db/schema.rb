2f042b2a2a7e52a996679ada0b529f67e77a2c05
iFabsss <guarinfabian7@gmail.com>
2026-02-02 16:19:56 +0800

    Cache Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_075445) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_02_081743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -75,12 +75,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_075445) do
   end
 
   create_table "solid_cache_entries", force: :cascade do |t|
+    t.integer "byte_size", null: false
     t.datetime "created_at", null: false
-    t.string "key"
-    t.string "key_hash"
-    t.datetime "updated_at", null: false
-    t.text "value"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash"
+    t.binary "key", null: false
+    t.bigint "key_hash", null: false
+    t.binary "value", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "tables", force: :cascade do |t|
